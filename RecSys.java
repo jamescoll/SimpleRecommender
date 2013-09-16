b5d696681d403c99871af9bd23860edab4dc5e84
JamesColl <james.evin.coll@gmail.com>
2013-09-16 15:33:24 +0100

    Updated code to include advanced formula

--- RecSys.java
+++ RecSys.java
@@ -201,78 +201,140 @@ class NonPersonalisedRecommender {
 			}
 		}
 
-		//hashmaps containing all the coRatings for movie A, movie B and movie C
+		// hashmaps containing all the coRatings for movie A, movie B and movie
+		// C
 		HashMap<Movie, Double> coRatingsA = new HashMap<Movie, Double>();
 		HashMap<Movie, Double> coRatingsB = new HashMap<Movie, Double>();
 		HashMap<Movie, Double> coRatingsC = new HashMap<Movie, Double>();
-		
-		//treemaps which impose ordering on those HashMaps this WILL FAIL if there are duplicate values
+
+		// hashmaps containing all the coRatings for movie A, movie B and movie
+		// C
+		HashMap<Movie, Double> coRatingsAdvancedA = new HashMap<Movie, Double>();
+		HashMap<Movie, Double> coRatingsAdvancedB = new HashMap<Movie, Double>();
+		HashMap<Movie, Double> coRatingsAdvancedC = new HashMap<Movie, Double>();
+
+		// treemaps which impose ordering on those HashMaps this WILL FAIL if
+		// there are duplicate values
 		TreeMap<Double, Movie> sortedA = new TreeMap<Double, Movie>();
 		TreeMap<Double, Movie> sortedB = new TreeMap<Double, Movie>();
 		TreeMap<Double, Movie> sortedC = new TreeMap<Double, Movie>();
 		
-		//getting the coRatings
-		populateCoRatings(movieA, movieMap, coRatingsA);
-		populateCoRatings(movieB, movieMap, coRatingsB);
-		populateCoRatings(movieC, movieMap, coRatingsC);
+		//treemaps to contain advanced co-Ratings - same thing as above applies
+		TreeMap<Double, Movie> sortedAdA = new TreeMap<Double, Movie>();
+		TreeMap<Double, Movie> sortedAdB = new TreeMap<Double, Movie>();
+		TreeMap<Double, Movie> sortedAdC = new TreeMap<Double, Movie>();
 
-		//sorting them by copying them into TreeMaps
-		
+		// we need the number of users for the second formula
+		int numUsers = userMap.size();
+
+		// getting the coRatings
+		populateCoRatings(movieA, movieMap, coRatingsA, coRatingsAdvancedA,
+				numUsers);
+		populateCoRatings(movieB, movieMap, coRatingsB, coRatingsAdvancedB,
+				numUsers);
+		populateCoRatings(movieC, movieMap, coRatingsC, coRatingsAdvancedC,
+				numUsers);
+
+		// sorting them by copying them into TreeMaps
 		sortedA = sortCoRatings(coRatingsA);
 		sortedB = sortCoRatings(coRatingsB);
 		sortedC = sortCoRatings(coRatingsC);
 		
-	
+		sortedAdA = sortCoRatings(coRatingsAdvancedA);
+		sortedAdB = sortCoRatings(coRatingsAdvancedB);
+		sortedAdC = sortCoRatings(coRatingsAdvancedC);
+		
 
 		// Write the top 5 movies, one per line, to a text file.
 		try {
 			PrintWriter writer = new PrintWriter("pa1-result.txt", "UTF-8");
 
-			int counter=0;
-			
+			int counter = 0;
+
 			Iterator<Double> movieASetIterator = sortedA.keySet().iterator();
 			writer.print(movieA);
-			while(movieASetIterator.hasNext()&&counter!=5)
-			{
-				
+			while (movieASetIterator.hasNext() && counter != 5) {
+
 				Double key = movieASetIterator.next();
 				writer.print("," + sortedA.get(key).getMovieId());
 				double a = round(key, 2);
 				writer.print("," + a);
 				counter++;
-				
+
 			}
 			writer.print("\n");
-			
+
 			counter = 0;
 			Iterator<Double> movieBSetIterator = sortedB.keySet().iterator();
 			writer.print(movieB);
-			while(movieBSetIterator.hasNext()&&counter!=5)
-			{
-				
+			while (movieBSetIterator.hasNext() && counter != 5) {
+
 				Double key = movieBSetIterator.next();
 				writer.print("," + sortedB.get(key).getMovieId());
 				double a = round(key, 2);
 				writer.print("," + a);
 				counter++;
-				
+
 			}
 			writer.print("\n");
-			
+
 			counter = 0;
 			Iterator<Double> movieCSetIterator = sortedC.keySet().iterator();
 			writer.print(movieC);
-			while(movieCSetIterator.hasNext()&&counter!=5)
-			{
-				
+			while (movieCSetIterator.hasNext() && counter != 5) {
+
 				Double key = movieCSetIterator.next();
 				writer.print("," + sortedC.get(key).getMovieId());
 				double a = round(key, 2);
 				writer.print("," + a);
 				counter++;
-				
+
 			}
 			writer.print("\n");
+			
+			counter = 0;
+
+			Iterator<Double> movieA1SetIterator = sortedAdA.keySet().iterator();
+			writer.print(movieA);
+			while (movieA1SetIterator.hasNext() && counter != 5) {
+
+				Double key = movieA1SetIterator.next();
+				writer.print("," + sortedAdA.get(key).getMovieId());
+				double a = round(key, 2);
+				writer.print("," + a);
+				counter++;
+
+			}
+			writer.print("\n");
+
+			counter = 0;
+			Iterator<Double> movieB1SetIterator = sortedAdB.keySet().iterator();
+			writer.print(movieB);
+			while (movieB1SetIterator.hasNext() && counter != 5) {
+
+				Double key = movieB1SetIterator.next();
+				writer.print("," + sortedAdB.get(key).getMovieId());
+				double a = round(key, 2);
+				writer.print("," + a);
+				counter++;
+
+			}
+			writer.print("\n");
+
+			counter = 0;
+			Iterator<Double> movieC1SetIterator = sortedAdC.keySet().iterator();
+			writer.print(movieC);
+			while (movieC1SetIterator.hasNext() && counter != 5) {
+
+				Double key = movieC1SetIterator.next();
+				writer.print("," + sortedAdC.get(key).getMovieId());
+				double a = round(key, 2);
+				writer.print("," + a);
+				counter++;
+
+			}
+			writer.print("\n");
+			
 			
 
 			writer.close();
@@ -281,26 +343,30 @@ class NonPersonalisedRecommender {
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	//THIS IS NOT THE OPTIMAL SOLUTION.. IN THE UNLIKELY EVENT (FOR LARGE DATA)
-	//THAT THERE ARE DUPLICATE RATINGS VALUES - THIS WILL BREAK
+
+	// THIS IS NOT THE OPTIMAL SOLUTION.. IN THE UNLIKELY EVENT (FOR LARGE DATA)
+	// THAT THERE ARE DUPLICATE RATINGS VALUES - THIS WILL BREAK
 	// DUPLICATES ON ZERO ARE ALSO POSSIBLE AND MORE LIKELY
-	public static TreeMap<Double, Movie> sortCoRatings(HashMap<Movie, Double> coRatings){
-		//this function takes a list of CoRatings and sorts it according to the double value
-		TreeMap<Double, Movie> sortedMap = new TreeMap<Double, Movie>(Collections.reverseOrder());
-		//to do this we will effectively flip the HashMap into a TreeMap on which we can compare values
-		for(Map.Entry<Movie, Double> entry:coRatings.entrySet())
-		{
+	public static TreeMap<Double, Movie> sortCoRatings(
+			HashMap<Movie, Double> coRatings) {
+		// this function takes a list of CoRatings and sorts it according to the
+		// double value
+		TreeMap<Double, Movie> sortedMap = new TreeMap<Double, Movie>(
+				Collections.reverseOrder());
+		// to do this we will effectively flip the HashMap into a TreeMap on
+		// which we can compare values
+		for (Map.Entry<Movie, Double> entry : coRatings.entrySet()) {
 			Movie m = entry.getKey();
 			Double d = entry.getValue();
 			sortedMap.put(d, m);
 		}
 		return sortedMap;
-		
+
 	}
 
 	public static void populateCoRatings(int inputMovie,
-			HashMap<Integer, Movie> movieMap, HashMap<Movie, Double> coRatings) {
+			HashMap<Integer, Movie> movieMap, HashMap<Movie, Double> coRatings,
+			HashMap<Movie, Double> coRatingsAdvanced, int numUsers) {
 		// for each rating belonging to the specific film
 		for (Rating r : movieMap.get(inputMovie).getRatings()) {
 
@@ -325,6 +391,25 @@ class NonPersonalisedRecommender {
 			}
 
 		}
+
+		// remove the input movie from the coRatings map
+		// Movie m = movieMap.get(inputMovie);
+		// coRatings.remove(m);
+
+		double div = numUsers - movieMap.get(inputMovie).getRatings().size();
+
+		// the denominator of the function which gives us x+y/x//!x+y/!x
+		// we can get the numerator after calculating the final coRating value
+		for (Map.Entry<Movie, Double> entry : coRatings.entrySet()) {
+			Movie m1 = entry.getKey();
+			// this is the number of people who have rated given movie but not
+			// input movie
+			double a = m1.getRatings().size() - coRatings.get(m1);
+			double b = a / div;
+			coRatingsAdvanced.put(m1, b);
+
+		}
+
 		// this will calculate the final co-rating value and save some work in
 		// main
 		Iterator<Integer> movieSetIterator = movieMap.keySet().iterator();
@@ -334,14 +419,26 @@ class NonPersonalisedRecommender {
 				double a = coRatings.get(movieMap.get(key));
 				double b = movieMap.get(inputMovie).getRatings().size() * 1.0;
 				double c = a / b;
-				//double d = round(c, 2);
+				// double d = round(c, 2);
 				coRatings.put(movieMap.get(key), c);
 			} else {
 				coRatings.put(movieMap.get(key), 0.0);
 			}
 
 		}
-		
+
+		// now we can work out the advanced coRatings value
+		for (Map.Entry<Movie, Double> entry : coRatingsAdvanced.entrySet()) {
+			Movie m1 = entry.getKey();
+			// this is the number of people who have rated given movie but not
+			// input movie
+			double a = coRatings.get(m1);
+			double b = coRatingsAdvanced.get(m1).doubleValue();
+			double c = a / b;
+			coRatingsAdvanced.put(m1, c);
+
+		}
+
 	}
 
 	// this will allow us to round the double values
